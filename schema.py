f9e2d3a25898e7fb07a857ba2687b45717a4e9bc
dev241202@gmail.com <dev241202@gmail.com>
2024-05-18 20:11:14 +0530

    Integrated DynamoDB scan and put_item

--- schema.py
+++ schema.py
@@ -1,6 +1,9 @@
 import typing
 import strawberry
+import boto3
 
+dynamodb = boto3.client('dynamodb', region_name='ap-south-1')
+table_name = 'studentTable'
 @strawberry.type
 class Student:
     name:str
@@ -12,28 +15,17 @@ class Query:
     students:typing.List[Student]
 
 def get_students():
-    return [
-        Student(
-            name="DR",
-            roll_no="56",
-            div="D20A"
-        ),
-        Student(
-            name="SL",
-            roll_no="36",
-            div="D20A"
-        ),
-                Student(
-            name="MK",
-            roll_no="28",
-            div="D20A"
-        ),
-                Student(
-            name="SK",
-            roll_no="24",
-            div="D20A"
-        ),
-    ]
+    response = dynamodb.scan(TableName=table_name)
+    items = response.get('Items', [])
+    students = []
+    for item in items:
+        name = item.get('name', {}).get('S', '')  
+        roll_no = int(item.get('roll_no', {}).get('N', '0'))  
+        div = item.get('div', {}).get('S', '') 
+        student = Student(name=name, roll_no=roll_no, div=div)
+        students.append(student)
+    return students
+
 
 @strawberry.type
 class Query:
@@ -44,7 +36,8 @@ class Mutation:
     @strawberry.mutation
     def add_student(self, name: str, roll_no: int, div:str) -> Student:
         print(f"Adding Name : {name}, Roll No : {roll_no}, Div : {div}")
- 
+        item = {'name': {'S':name}, 'roll_no': {'N':str(roll_no)}, 'div': {'S':div}}
+        dynamodb.put_item(TableName=table_name,Item=item)
         return Student(name=name, roll_no=roll_no, div=div)
 
 schema = strawberry.Schema(query=Query,mutation=Mutation)
